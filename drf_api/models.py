e1aa9292d1bdd647c1e1a9631155a453ff937f43
Simpola <ashukumarsinghrathore123@gmail.com>
2022-02-12 19:53:17 +0530

    first commit

---
+++ drf_api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Task(models.Model):
+    task = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+
+    def __str(self):
+        return self.task
